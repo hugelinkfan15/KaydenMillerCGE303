51417269092552dcce6a566b9abf26f9d1332906
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-02-16 12:52:35 -0600

    Platform
    Made platform script and lose script

--- Prototype/Assets/Scripts/LoseTriggerScript.cs
+++ Prototype/Assets/Scripts/LoseTriggerScript.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LoseTriggerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text output;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
-           
+        {
+            ScoreManager.gameOver = true;
+        };
     }
 }
