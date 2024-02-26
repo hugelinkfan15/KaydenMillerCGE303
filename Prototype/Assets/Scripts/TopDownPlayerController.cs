9b45d35153001abd939f08efb81ded439e9a952a
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-02-26 12:35:24 -0600

    more prefabs

---
+++ Prototype/Assets/Scripts/TopDownPlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        // Get the Rigidbody2D componet attached to the Gameobject
+        rb = GetComponent<Rigidbody2D>();   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Get input values for horizontal and vertical movement
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Move the player using Rigidbody2D in FixedUpdate
+        rb.velocity = new Vector2(movement.x*moveSpeed, movement.y*moveSpeed);
+    }
+}
